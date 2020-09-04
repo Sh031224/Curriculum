4305a98d6b35686f10c4e1ae73d3bc1c32868b8a
Sh031224 <1cktmdgh2@gmail.com>
2020-09-04 20:35:17 +0900

    [ADD] data.split

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -10,7 +10,18 @@ interface HomeProps {
 }
 
 const Home = ({ data }: HomeProps) => {
-  return <div className="home">{data.position}</div>;
+  return (
+    <div className="home">
+      <div className="home-position">{data.position}</div>
+      <div className="home-title">
+        <h1 className="home-title-name">
+          {data.name.split(" ").map((value: string, index: number) => {
+            console.log(value);
+          })}
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
